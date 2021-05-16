276ef6f0588839b27c2c51c0fbcda971a7198796
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-05-15 20:14:44 -0400

    Improve searching for ld.lld

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
 use std::env;
-use std::process::Command;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+
+fn exists(command: &str) -> bool {
+    Command::new(command)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .is_ok()
+}
+
+fn llvm_path() -> Option<PathBuf> {
+    let brew_output = Command::new("brew")
+        .args(&["--prefix", "llvm"])
+        .output()
+        .ok()?;
+
+    if brew_output.status.success() {
+        Some(Path::new(std::str::from_utf8(&brew_output.stdout).ok()?).join("bin"))
+    } else {
+        None
+    }
+}
+
+fn brew_path() -> Option<PathBuf> {
+    let path = llvm_path()?.join("ld.lld");
+
+    if path.exists() {
+        Some(path)
+    } else {
+        None
+    }
+}
+
+fn find_lld() -> Option<String> {
+    if exists("ld.lld") {
+        Some("ld.lld".to_string())
+    } else if Path::new("/usr/bin/ld.lld").exists() {
+        Some("/usr/bin/ld.lld".to_string())
+    } else if let Some(path) = brew_path() {
+        Some(path.to_string_lossy().to_string())
+    } else {
+        None
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args()
@@ -16,7 +61,9 @@ fn main() {
         .flatten()
         .collect();
 
-    let status = Command::new("ld.lld").args(&args).status().unwrap();
+    let ldd = find_lld().expect("ld.lld not found");
+
+    let status = Command::new(&ldd).args(&args).status().unwrap();
 
     if let Some(x) = status.code() {
         std::process::exit(x);
